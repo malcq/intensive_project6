249bcde2f75a9a1540e44846233e47b3967315bf
malcq <malcqq@gmail.com>
2021-03-02 03:35:59 +0300

    Added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,46 @@
-import { Link } from "react-router-dom";
 
-const Header = () => (
-  <header className="header">
-    <nav>
-      <Link to="/">Home</Link><br />
-      <Link to="/barcard">Bar card</Link><br />
-      <Link to="/recipes">Recipes</Link><br />
-      <Link to="/calendar">Calendar</Link><br />
-    </nav>
-  </header>
-);
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Link from './Link';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/">Home</Link>
+            <Link to="/barcard">Bar card</Link>
+            <Link to="/recipes">Recipes</Link>
+            <Link to="/calendar">Calendar</Link>
+          </Typography>          
+        </Toolbar>
+      </AppBar>
+    </header>);
+};
 
 export default Header;
